da651a2664fa4ba153645466e364fc6a1d0ad8db
ViktorLilja <viktorlilja2@gmail.com>
2023-10-06 23:40:23 +0200

    Identified mystery spectra and errorbar attempt

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import pandas as pd
 from mie_gans_spectra import get_mie_gans_crossection
 from universal_model_spectra import get_disk_crossection
 from dielectric_functions import *
-from scipy.optimize import minimize
+from scipy.optimize import curve_fit
 import plot_functions as f
 import os
 
 # Parameters
 nominal_density = 7.125 # particles per um2 from lab pm picture
-assumed_eps_medium = 1.26
+assumed_eps_medium = 1.26 # From universal model paper
 
 # --- MEASURED SPECTRA --- #
 data = np.genfromtxt("Data from lab/Part1_Group1_20230927_no_footer.csv", delimiter=",", skip_header=2)
@@ -50,35 +50,51 @@ fitted_data = {
     "C": {"metal": "Ag", "diameter": 140, "height": 15, "density": nominal_density},
     "D": {"metal": "Au", "diameter":  99, "height": 30, "density": nominal_density},
     "E": {"metal": "Cu", "diameter": 140, "height": 10, "density": nominal_density},
+    "F": {"metal": "Cu", "diameter": 140, "height": 30, "density": nominal_density},
+    "G": {"metal": "Ag", "diameter": 140, "height": 30, "density": nominal_density},
 }
 
-def chi2_error(params, *args):
+def error(params, *args):
     diameter, height, density = params
     metal, wl, extinction = args
     eps1_metal, eps2_metal   = eps_metals(wl, metal)
     eps1_medium, eps2_medium = eps_constant(wl, assumed_eps_medium) 
     theoretical_extinction = 1e-6 * density * get_disk_crossection(wl, eps1_metal, eps2_metal, eps1_medium, diameter, height)
-    return np.sqrt(np.sum((extinction - theoretical_extinction)**2))
+    
+    
+    fit_indices = (wl > 500) & (wl < 1000)
+    
+    return extinction[fit_indices] - theoretical_extinction[fit_indices]
 
 for sample, particle in fitted_data.items():
-
     wl_measured  = spectra[sample][:,0]
     ext_measured = spectra[sample][:,1]
 
-    x0 = np.array([particle["diameter"], particle["height"], nominal_density])
-    args = (particle["metal"], wl_measured, ext_measured)
-    res = minimize(chi2_error, x0, args=args, tol=1e-6)
-    print(res.message)
-    diameter, height, density = res.x
+    # Exclude very noisy regions
+    fit_indices = (wl_measured > 500) & (wl_measured < 1000)
+    wl_measured = wl_measured[fit_indices]
+    ext_measured = ext_measured[fit_indices]
+
+    # Define model on format that is acceptable by curve_fit
+    eps1_metal, eps2_metal   = eps_metals(wl_measured, particle["metal"])
+    eps1_medium, eps2_medium = eps_constant(wl_measured, assumed_eps_medium)
+    curve_fit_model = lambda wavelength, diameter, height, density: 1e-6 * density * get_disk_crossection(wavelength, eps1_metal, eps2_metal, eps1_medium, diameter, height)
+
+    # Fit model and print result
+    p0 = np.array([particle["diameter"], particle["height"], nominal_density])
+    popt, pcov = curve_fit(curve_fit_model, wl_measured, ext_measured, p0)
+    print(f"Sample {sample}: D=({popt[0]:5.1f}±{np.sqrt(pcov[0,0]):3.1f}) nm, h=({popt[1]:4.1f}±{np.sqrt(pcov[1,1]):3.1f}) nm, N=({popt[2]:4.1f}±{np.sqrt(pcov[2,2]):3.1f}) um^-2")
+    
+    # Updated fitted parameters
+    diameter, height, density = popt
     particle["diameter"] = diameter
     particle["height"] = height
     particle["density"] = density
 
+    # Save higher resolution fitted spectra
     wl = np.linspace(300, 1100, 1000)
-
     eps1_metal, eps2_metal   = eps_metals(wl, particle["metal"])
     eps1_medium, eps2_medium = eps_constant(wl, assumed_eps_medium)
-
     ext = 1e-6 * density * get_disk_crossection(
         wl, eps1_metal, eps2_metal, eps1_medium, diameter, height)
     
@@ -130,4 +146,47 @@ for i, ax in enumerate(axs):
 f.align_labels(fig)
 f.set_layout_tight(fig)
 f.export_figure_as_pdf(os.path.abspath(os.path.dirname(__file__)) + "\\Figure\\TIF120_LSPR_measured_theoretical_spectra.pdf")
+plt.show()
+
+
+# Plotting Mystery samples
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(16/2.54, 10/2.54), sharex=True, sharey=False)
+
+for i, sample in enumerate(["F", "G"]):
+
+    color = ["C5", "C6"][i]
+    wl_measured  = spectra[sample][:,0]
+    ext_measured = spectra[sample][:,1]
+    axs[i].plot(wl_measured, ext_measured, color, label=f"Measured, Sample {sample}")
+
+    reference_sample = ["E", "C"][i]
+    reference_color  = ["C4", "C2"][i]
+    wl_measured  = spectra[reference_sample][:,0]
+    ext_measured = spectra[reference_sample][:,1]
+    axs[i].plot(wl_measured, ext_measured, reference_color, label=f"Measured, Sample {reference_sample}")
+
+    wl_fitted = fitted_data[sample]["wl"]
+    ext_fitted = fitted_data[sample]["ext"]
+    axs[i].plot(wl_fitted, ext_fitted, "k--", label=f'Calculated, {fitted_data[sample]["metal"]}, $D={fitted_data[sample]["diameter"]:3.0f}$\,nm, $h={fitted_data[sample]["height"]:2.0f}$\,nm')
+
+
+x_labels = ["", "Wavelength / nm"]
+y_labels = ["", "Normalized extinction"]
+x_lims   = [(None, None), (280,1120)]
+y_lims   = [(-0.05, 1.05), (-0.05, 1.05)]
+
+
+for i, ax in enumerate(axs):
+    #f.set_axis_scale(   axs, xScale_string='linear', yScale_string='linear')
+    f.set_axis_labels(  ax, x_label=x_labels[i], y_label=y_labels[i])
+    #f.set_axis_invert(  ax, x_invert=False, y_invert=False)
+    f.set_axis_limits(  ax, x_lims[i][0], x_lims[i][1], y_lims[i][0], y_lims[i][1])
+    f.set_grid(         ax, grid_major_on=True, grid_major_linewidth=0.7, grid_minor_on=False, grid_minor_linewidth=0.3) # set_grid must be after set_axis_scale for some reason (at least with 'log')
+    f.set_legend(       ax, legend_on=True, alpha=1.0, location='best')
+    ax.yaxis.set_ticklabels([]) # remove labels but keep tick marks
+
+
+f.align_labels(fig)
+f.set_layout_tight(fig)
+f.export_figure_as_pdf(os.path.abspath(os.path.dirname(__file__)) + "\\Figure\\TIF120_LSPR_mystery_spectra.pdf")
 plt.show()
